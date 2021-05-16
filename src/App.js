35436bba466077ff2a7a5cbe68e7f188e7082335
Mijan Uddin Ahmed <63743944+mijanuahmed@users.noreply.github.com>
2021-05-16 21:52:20 +0100

    movie-watchlist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Watchlist from "./components/Watchlist";
+import Watched from "./components/Watched";
+import Add from "./components/Add";
+import GlobalProvider from "./context/GlobalState";
+import "./lib/font-awesome/css/all.min.css";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Watchlist />
+          </Route>
+          <Route path="/watched">
+            <Watched />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </GlobalProvider>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="content-wrap">
+      <div className="footer">
+        <p>© MIJAN AHMED. Contact: mijanuahmed@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
